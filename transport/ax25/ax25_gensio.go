32692684e12f2dcf8f20b430490f61e3a4fbac69
Corey Minyard <cminyard@mvista.com>
2022-03-22 07:16:37 -0500

    ax25:gensio: Add beacon support.
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- transport/ax25/ax25_gensio.go
+++ transport/ax25/ax25_gensio.go
@@ -90,7 +90,8 @@ func getBaseGensio(gensiostr, mycall string) (g gensio.Gensio, err error) {
 		s := fmt.Sprintf("ax25(laddr=%s),%s", mycall, gensiostr)
 		g = gensio.NewGensio(s, gax25o, &gevent{})
 		g.OpenS()
-		gax25str = s
+		gax25str = gensiostr
+		gax25call = mycall
 		gax25 = g
 		gax25_refcount = 1
 		go gLoop()
@@ -266,7 +267,6 @@ func (l *Listener) destroy() {
 }
 
 func ListenGensioAX25(gensiostr, mycall string) (rc net.Listener, err error) {
-	fmt.Printf("Setting up listener: '%s', '%s'\n", gensiostr, mycall)
 	l := &Listener{}
 	l.quit = make(chan bool)
 	l.localAddr = mycall
@@ -286,8 +286,56 @@ func ListenGensioAX25(gensiostr, mycall string) (rc net.Listener, err error) {
 	return l, nil
 }
 
-var ErrNoGensioSupport = errors.New("Gensio AX.25 support not ready yet")
 
-func NewGensioAX25Beacon(axPort, mycall, dest, message string) (Beacon, error) {
-	return nil, ErrNoGensioSupport
+type gensioAX25Beacon struct {
+	message string
+	localAddr string
+	remoteAddr string
+	g gensio.Gensio
+}
+
+func NewGensioAX25Beacon(gensiostr, mycall, dest, message string) (rb Beacon, err error) {
+	b := &gensioAX25Beacon{}
+	b.localAddr = mycall
+	b.remoteAddr = dest
+	b.message = message
+	b.g, err = getBaseGensio(gensiostr, mycall)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (b *gensioAX25Beacon) Message() string {
+	return b.message
+}
+
+func (b *gensioAX25Beacon) LocalAddr() net.Addr {
+	return gensio.NewAx25NetAddr(b.localAddr)
+}
+
+func (b *gensioAX25Beacon) RemoteAddr() net.Addr {
+	return gensio.NewAx25NetAddr(b.remoteAddr)
+}
+
+func (b *gensioAX25Beacon) Every(d time.Duration) error {
+	for {
+		if err := b.Now(); err != nil {
+			return err
+		}
+		time.Sleep(d)
+	}
+}
+
+func (b *gensioAX25Beacon) Now() (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%s", r)
+		}
+	}()
+
+	b.g.Write([]byte(b.message),
+		[]string{"oob", fmt.Sprintf("addr:0,%s,%s", b.remoteAddr, b.localAddr)})
+
+	return nil
 }
